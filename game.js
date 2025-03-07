7d5849f19c55474af017bbf44795eb1478ab6e41
Laith Abushanab <6297085@edu.vaniercollege.qc.ca>
2025-03-06 20:52:20 -0500

    Deleted game.js

--- game.js
+++
@@ -1,7 +0,0 @@
-const panel = document.getElementById("panel");
-const sound = new Audio("sounds/door.mp3");
-
-panel.onclick = function() {
-     this.classList.toggle("slide-up");
-     sound.play();
-}
